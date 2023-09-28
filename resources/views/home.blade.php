7af17f0929f17d01803e1c108b37052c5eedc253
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-09-28 10:28:35 +0200

    un po di stile a caso

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,30 @@
 
 @section('content')
     <div class="container">
-        @foreach ($trains as $train)
-            <div class="card-left">
-                <p>{{ $train->azienda }}</p>
-                <p>{{ $train->stazione_di_partenza }}</p>
-                <p>{{ $train->orario_di_partenza }}</p>
-                <p>{{ $train->orario_di_arrivo }}</p>
-                <p>{{ $train->codice_treno}}</p>
-                <p>{{ $train->numero_carrozze }}</p>
-            </div>
-        @endforeach
+        <table>
+            <tr>
+              <th>Compagnia</th>
+              <th>Stazione di partenza</th>
+              <th>Orario di partenza</th>
+              <th>Orario di arrivo</th>
+              <th>Data di partenza</th>
+              <th>Codice treno</th>
+              <th>Carrozza N°</th>
+            </tr>
+            @foreach ($trains as $train)
+            <tr>
+              <td>{{ $train->azienda }}</td>
+              <td>{{ $train->stazione_di_partenza }}</td>
+              <td>{{ $train->orario_di_partenza }}</td>
+              <td>{{ $train->orario_di_arrivo }}</td>
+              <td>{{ $train->data_di_partenza }}</td>
+              <td>{{ $train->codice_treno}}</td>
+              <td>{{ $train->numero_carrozze }}</td>
+
+            </tr>
+           
+            @endforeach
+          </table> 
+       
     </div>
+</div>
